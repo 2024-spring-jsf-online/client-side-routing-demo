ae3c408761bb37b22669bb5a302a93f72eef65d8
tsteelematc <tsteele@madisoncollege.edu>
2023-12-02 13:22:15 -0600

    Generated two components...
    ng generate component world --skip-tests
    
    ng generate component kitty --skip-tests

---
+++ src/app/kitty/kitty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kitty',
+  standalone: true,
+  imports: [],
+  templateUrl: './kitty.component.html',
+  styleUrl: './kitty.component.css'
+})
+export class KittyComponent {
+
+}
